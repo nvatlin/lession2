5bc71fe2b08f3d1ed67d564e4a3aa726ae258e4e
nvatlin <nvatlin91@gmail.com>
2024-10-30 12:16:23 +0500

    Add files via upload

--- базовые структуры данных.py
+++ базовые структуры данных.py
@@ -5,10 +5,10 @@ print((9 ** 0.5) * 5)
 print(9.99 > 9.98 and 1000 != 1000.1 )
 
 # 3rd program
-print(2*2+2)
-print(2*(2+2))
-print(2*2+2 == 2*(2+2))
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print(2 * 2 + 2 == 2 * (2 + 2))
 
 # 4th program
-print (int(float("123.456")*10%10))
+print (int(float("123.456") * 10 % 10))
 
